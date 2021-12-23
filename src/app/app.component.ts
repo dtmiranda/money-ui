bad8d43ac84f166fee85f7d26ceae8656444ab95
dtmiranda <hortadito2@gmail.com>
2021-12-23 12:16:13 -0100

    data table implentation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'money-ui';
+  lancamentos=[
+    {tipo: 'DESPESAS', descricao: 'compra de pao', dataVencimento:'30/06/2021', dataPagamento: null, valor:5.44, pessoa:'Padaria do Jose'},
+    {tipo: 'DESPESAS', descricao: 'compra de pao', dataVencimento:'30/06/2021', dataPagamento: null, valor:5.44, pessoa:'Padaria do Jose'},
+    {tipo: 'DESPESAS', descricao: 'compra de pao', dataVencimento:'30/06/2021', dataPagamento: null, valor:5.44, pessoa:'Padaria do Jose'},
+    {tipo: 'DESPESAS', descricao: 'compra de pao', dataVencimento:'30/06/2021', dataPagamento: null, valor:5.44, pessoa:'Padaria do Jose'},
+    {tipo: 'DESPESAS', descricao: 'compra de pao', dataVencimento:'30/06/2021', dataPagamento: null, valor:5.44, pessoa:'Padaria do Jose'},
+    {tipo: 'DESPESAS', descricao: 'compra de pao', dataVencimento:'30/06/2021', dataPagamento: null, valor:5.44, pessoa:'Padaria do Jose'},
+    {tipo: 'DESPESAS', descricao: 'compra de pao', dataVencimento:'30/06/2021', dataPagamento: null, valor:5.44, pessoa:'Padaria do Jose'},
+    {tipo: 'DESPESAS', descricao: 'compra de pao', dataVencimento:'30/06/2021', dataPagamento: null, valor:5.44, pessoa:'Padaria do Jose'},
+    {tipo: 'DESPESAS', descricao: 'compra de pao', dataVencimento:'30/06/2021', dataPagamento: null, valor:5.44, pessoa:'Padaria do Jose'},
+    {tipo: 'DESPESAS', descricao: 'compra de pao', dataVencimento:'30/06/2021', dataPagamento: null, valor:5.44, pessoa:'Padaria do Jose'},
+  ]
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { InputTextModule } from 'primeng/inputtext';
 import {ButtonModule} from 'primeng/button';
+import { TableModule } from 'primeng/table'
 
 
 import { AppComponent } from './app.component';
@@ -15,6 +17,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     InputTextModule,
     ButtonModule,
+    TableModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
